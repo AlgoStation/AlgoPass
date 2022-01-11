9de2f569bd7e63b57a7dfa8ac89c762d2df60100
pdemicheli <pietro@demicheli.com>
2022-01-11 12:31:33 +0000

    Auto form submission!

--- script.js
+++ script.js
@@ -72,6 +72,8 @@ const getCardHTML = (id, site, uname) => {
 const getOptCardHTML = async () => {
 	let url = `https://docs.google.com/forms/d/e/1FAIpQLSf4KevLfPtzSVifLEQ0Ryjz5ZhpzAvWy2WzgCjtpTOvD4zJ1g/viewform?usp=pp_url&entry.769399060=${await algo.getAddr()}`;
 
+	// let url = `https://docs.google.com/forms/d/e/1FAIpQLSf4KevLfPtzSVifLEQ0Ryjz5ZhpzAvWy2WzgCjtpTOvD4zJ1g/formResponse?usp=pp_url&entry.769399060=${await algo.getAddr()}&submit=Submit`;
+
 	return `
 	<div class="card" id="opt-in-card">
 	<svg id="proceed-btn" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"/></svg>
